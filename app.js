d76f5752c619e0a250216f4bf6831d8f71bc4246
Harshit Shukla <125036743+vaasuuu@users.noreply.github.com>
2023-09-17 02:13:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const v1 = document.getElementById("rr1");
+let f1 = 0;
+v1.addEventListener('click',func1);
+function func1() {
+    v1.style.backgroundColor = "#7A0000";
+    f1 = 1;
+}
+
+const v2 = document.getElementById("rr2");
+let f2=0;
+v2.addEventListener('click',func2);
+function func2() {
+    v2.style.backgroundColor = "#7A0000";
+    f2=1;
+}
+
+const v3 = document.getElementById("rr3");
+let f3=0;
+v3.addEventListener('click',func3);
+function func3() {
+    v3.style.backgroundColor = "#7A0000";
+    f3=1;
+}
+
+const v4 = document.getElementById("rr4");
+let f4=0;
+v4.addEventListener('click',func4);
+function func4() {
+    v4.style.backgroundColor = "#7A0000";
+    f4=1;
+}
+
+const v5 = document.getElementById("rr5");
+let f5=0;
+v5.addEventListener('click',func5);
+function func5() {
+    v5.style.backgroundColor = "#7A0000";
+    f5=1;
+}
+
+const v6 = document.getElementById("rr6");
+let f6=0;
+v6.addEventListener('click',func6);
+function func6() {
+    v6.style.backgroundColor = "#7A0000";
+    f6=1;
+}
+
+const v7 = document.getElementById("rr7");
+let f7=0;
+v7.addEventListener('click',func7);
+function func7() {
+    v7.style.backgroundColor = "#7A0000";
+    f7=1;
+}
+
+const v8 = document.getElementById("rr8");
+let f8=0;
+v8.addEventListener('click',func8);
+function func8() {
+    v8.style.backgroundColor = "#7A0000";
+    f8=1;
+}
+
+const v9 = document.getElementById("rr9");
+let f9=0;
+v9.addEventListener('click',func9);
+function func9() {
+    v1.style.backgroundColor = "#00407A";
+    v2.style.backgroundColor = "#00407A";
+    v3.style.backgroundColor = "#00407A";
+    v4.style.backgroundColor = "#00407A";
+    v5.style.backgroundColor = "#00407A";
+    v6.style.backgroundColor = "#00407A";
+    v7.style.backgroundColor = "#00407A";
+    v8.style.backgroundColor = "#00407A";
+    f9=1;
+}
